6ef171fba221af7fb7c58ebebe23c36c80c99040
andrewcbuensalida <andrewcbuensalida@gmail.com>
2022-02-13 15:19:30 -0800

    Payment class finished

--- public/classes/Payment.js
+++ public/classes/Payment.js
@@ -1,1 +1,10 @@
-"use strict";
+export class Payment {
+    constructor(recipient, details, amount) {
+        this.recipient = recipient;
+        this.details = details;
+        this.amount = amount;
+    }
+    format() {
+        return `${this.recipient} paid $${this.amount} for ${this.details}`;
+    }
+}
