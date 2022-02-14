2a51d7e6c6f3b7ada61d2cafb2e0399e376b5027
andrewcbuensalida <andrewcbuensalida@gmail.com>
2022-02-14 01:09:59 -0800

    manually formatted timestamp because couldn't figure out how to import date-fns into typescript

--- client/public/classes/ListTemplate.js
+++ client/public/classes/ListTemplate.js
@@ -7,11 +7,13 @@ export class ListTemplate {
     render(item, heading, pos) {
         const li = document.createElement("li");
         const top = document.createElement("div");
-        top.classList.add('topInvoice');
+        top.classList.add("topInvoice");
         const h4 = document.createElement("h4");
         h4.innerText = heading;
         const h4Time = document.createElement("h4");
-        h4Time.innerText = item.timeStamp;
+        const timeStampArray = item.timeStamp.split('T');
+        const formattedTimeStamp = timeStampArray[0] + ' ' + timeStampArray[1].slice(0, 5);
+        h4Time.innerText = formattedTimeStamp;
         top.append(h4);
         top.append(h4Time);
         li.append(top);
--- client/src/classes/ListTemplate.ts
+++ client/src/classes/ListTemplate.ts
@@ -1,23 +1,24 @@
 import { HasFormatter } from "../interfaces/HasFormatter";
-import { Invoice } from "./Invoice";
-import { Payment } from "./Payment";
- 
 
 export class ListTemplate {
 	// can do this format if there is an access modifier
 	constructor(private container: HTMLUListElement) {}
 
 	// start|end means it could either be 'start' or 'end'
-	render(item: Invoice|Payment, heading: string, pos: "start" | "end") {
+	render(item: HasFormatter, heading: string, pos: "start" | "end") {
 		const li = document.createElement("li");
-		const top = document.createElement("div")
-		top.classList.add('topInvoice')
+		const top = document.createElement("div");
+		top.classList.add("topInvoice");
 		const h4 = document.createElement("h4");
 		h4.innerText = heading;
 		const h4Time = document.createElement("h4");
-		h4Time.innerText = item.timeStamp
-		top.append(h4)
-		top.append(h4Time)
+		const timeStampArray = item.timeStamp.split('T')
+
+		const formattedTimeStamp = timeStampArray[0]+' '+timeStampArray[1].slice(0,5)
+
+		h4Time.innerText = formattedTimeStamp;
+		top.append(h4);
+		top.append(h4Time);
 		li.append(top);
 		const p = document.createElement("p");
 		p.innerText = item.format();
